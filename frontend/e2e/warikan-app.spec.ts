383d81b73f973ebe01961d25893e12448fd4db9f
hiroka <mitsunagahrk87@gmail.com>
2024-12-08 09:31:20 +0900

    67. グループ作成機能のE2Eテスト

--- frontend/e2e/warikan-app.spec.ts
+++ frontend/e2e/warikan-app.spec.ts
@@ -8,4 +8,28 @@ test.describe("割り勘アプリ", () => {
     //pageの遷移を行うためにpalywrightのpageオブジェクトを引数に置いておく
     await page.goto("http://localhost:3001");
   });
+
+  test.describe("グループ作成", () => {
+    test("グループが作成され支出登録ページに遷移する", async ({ page }) => {
+      const groupNameInput = page.getByLabel("グループ名");
+      await groupNameInput.fill("group1");
+      const memberListInput = page.getByLabel("メンバー");
+      await memberListInput.fill("太郎,花子");
+
+      const submitButton = page.getByRole("button");
+      await submitButton.click();
+
+      await expect(page).toHaveURL(/.+\/group\/group1/);
+    });
+    test("バリデーションエラーが存在する場合、グループ作成されず、ページ遷移しない", async ({
+      page,
+    }) => {
+      const submitButton = page.getByRole("button");
+      await submitButton.click();
+
+      await expect(page.getByText("グループ名は必須です")).toBeVisible();
+      await expect(page.getByText("メンバーは2人以上必要です")).toBeVisible();
+      await expect(page).not.toHaveURL(/.+\/group\/group1/);
+    });
+  });
 });
